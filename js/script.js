8d43dfded066d9a582777e47ba98bfaa69134556
shafi <shafiullaattar786@gmail.com>
2023-06-30 00:10:34 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let string = "";
+
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+
+        if (e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string ;
+        }
+
+        else if (e.target.innerHTML == '%')
+        {
+            if (string == '')
+            {
+                string= ""
+                document.querySelector('input').value = string ;
+            }
+            
+            else {
+
+                string = string/100 ;
+                document.querySelector('input').value = string ;
+            }
+        }
+        
+        else if (e.target.innerHTML == 'Del')
+        {
+            string = string.slice(0,-1);
+            document.querySelector('input').value = string ;
+        }
+        
+        else if (e.target.innerHTML == 'OFF')
+        {
+            string = "";
+            document.querySelector('input').value = string ;
+        }
+        
+        else if (e.target.innerHTML == 'AC')
+        {
+                string = ""
+                document.querySelector('input').value = string ;
+        }
+
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML ;
+            document.querySelector('input').value = string ;
+        }
+
+    })
+})
